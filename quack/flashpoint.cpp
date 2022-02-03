685aa6a1f1fc0928353102053767a07b46acc61d
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-02-03 03:36:31 +0000

    Added heartbeat functionality

--- quack/flashpoint.cpp
+++ quack/flashpoint.cpp
@@ -1,16 +1,63 @@
+#define _WINSOCKAPI_
 #include <Windows.h>
 #include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+#include "Lib/httplib.hpp"
+#include "Lib/json.hpp"
+
+// Make these library namespace names a bit nicer
+namespace http = httplib;
+namespace json = nlohmann;
+namespace thread = std::this_thread;
+namespace chrono = std::chrono;
+
+using namespace std::chrono_literals;
 
 int main() {
+    // Console for debug
     AllocConsole();
+    freopen_s(reinterpret_cast<FILE**>(stdin), "CONIN$", "r", stdin);
     freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
     freopen_s(reinterpret_cast<FILE**>(stderr), "CONOUT$", "w", stderr);
 
     const auto hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, 12);
 
-    std::cout << "Hello, world!" << '\n';
+    // Request testing
 
-    system("pause");
+
+    http::Client cli{ "http://localhost:7982" };
+    for (unsigned i = 1u; ; ++i) {
+        json::json body{};
+        body["heartbeat"] = {
+            {"id", "uuid1273198439343492237401"},
+            {"name", "legit_player"},
+            {"uptime", i},
+            {"blob", {
+                {"Game-Specific-Info", "Important data!"}
+            }}
+        };
+        SetConsoleTextAttribute(hConsole, 7);
+        std::cout << "\nSending heartbeat number " << i << "...\n";
+        SetConsoleTextAttribute(hConsole, 2);
+
+        if (auto res = cli.Post("/", body.dump(), "application/json")) {
+            if (res->status == 200) {
+                std::cout << res->body << std::endl;
+            }
+        }
+        else {
+            auto err = res.error();
+            std::cout << http::to_string(err) << std::endl;
+        }
+        thread::sleep_for(3s);
+    }
+
+    // Halt app to print error text
+    std::string dummy;
+    std::cin >> dummy;
     return 0;
 }

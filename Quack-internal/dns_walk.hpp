2d233aa1519e1cb3e17406c9dbb3548a490a6f41
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-03-30 23:59:57 +0100

    Refactor to .hpp

---
+++ Quack-internal/dns_walk.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "pch.h"
+
+
+struct DnsEntry {
+    std::wstring name{};
+    int type{};
+};
+
+std::optional<std::vector<DnsEntry>> GetCachedDNSData();
+std::optional<std::vector<DnsEntry>> CheckForBlacklistedDNSEntries();
---
+++ Quack-internal/framework.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX                        // Disable evil windows macro
+
+// Windows Header Files
+#include <Windows.h>

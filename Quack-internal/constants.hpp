ee3f7eddf6064ccd60c8230e4bc598d5799ffc42
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-04-04 15:29:53 +0100

    Begin identification logic

--- Quack-internal/constants.hpp
+++ Quack-internal/constants.hpp
@@ -5,7 +5,7 @@ namespace constants {
     const LPCWSTR W_DLL_NAME { L"Quack-internal" };
     const LPCSTR DLL_NAME{ "Quack-internal" };
 
-    const std::string VERSION { "0.3.6" };
+    const std::string VERSION { "0.3.7" };
     constexpr unsigned IPC_PORT = 5175; // Local machine communications port
     constexpr unsigned NET_PORT = 7982; // Foreign network communications port
     static constexpr bool DBG = true;
---
+++ Quack-internal/identification.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.hpp"
+
+#include "d3d9.h"
+#pragma comment(lib, "d3d9.lib")
+
+void GetGPUID() {
+
+    // https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemfirmwaretable
+    auto smbios = GetSystemFirmwareTable(
+        static_cast<int>('RSMB'),
+        0,
+        nullptr,
+        0
+    );
+
+    auto dummy_object = Direct3DCreate9(D3D_SDK_VERSION);
+    auto adapter_count = dummy_object->GetAdapterCount();
+    // todo: Hwid
+}

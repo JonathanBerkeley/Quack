4cbf52fa964d754d12d8b77f5387e4da6f83e6a9
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-04-21 23:32:21 +0100

    Find process in blacklist and terminate it

--- Testbed/main.cpp
+++ Testbed/main.cpp
@@ -5,10 +5,65 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <Windows.h>
 #include <Psapi.h>
+#include <TlHelp32.h>
 
 #include <iostream>
 #include <cstdio>
 #include <sstream>
+#include <optional>
+#include <unordered_map>
+#include <algorithm>
+#include <ranges>
+
+
+
+std::wstring wstring_to_lower(const std::wstring& wide_str) {
+
+    std::wstring output;
+
+    for (auto& wc : wide_str)
+        output += static_cast<wchar_t>(towlower(wc));
+
+    return output;
+}
+
+using ProcessEntry = std::unordered_map<DWORD, std::wstring>;
+std::optional<ProcessEntry> find_processes(const std::vector<std::wstring>& blacklist) {
+
+    ProcessEntry entries_found{};
+
+    PROCESSENTRY32 process_entry{};
+    process_entry.dwSize = sizeof(PROCESSENTRY32);
+
+    // ReSharper disable once CppLocalVariableMayBeConst
+    HANDLE proc_snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+    if (!Process32First(proc_snap, &process_entry))
+        return std::nullopt;
+
+    // Loop over processes and look for name
+    while (Process32Next(proc_snap, &process_entry)) {
+
+        std::wstring process_name{ process_entry.szExeFile };
+        // Create lower-string representations
+        process_name = wstring_to_lower(process_name);
+
+        // Check blacklist for entry
+        for (const auto& entry : blacklist)
+            if (process_name.find(entry) != std::wstring::npos)
+                // A process with a blacklisted name was found, so store it
+                entries_found[process_entry.th32ProcessID] = process_entry.szExeFile;
+
+    }
+
+    // Cleanup handle
+    CloseHandle(proc_snap);
+
+    if (entries_found.empty())
+        return std::nullopt;
+
+    return entries_found;
+}
 
 
 /**
@@ -22,7 +77,7 @@ void PrintProcessNameAndID(DWORD processID) {
     struct Process {
         TCHAR name[MAX_PATH];
         HANDLE handle;
-    } process {
+    } process{
         L"<Untitled>",
         OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,FALSE, processID)
     };
@@ -32,8 +87,9 @@ void PrintProcessNameAndID(DWORD processID) {
         HMODULE h_mod;
         DWORD cb_needed;
 
+
         if (EnumProcessModules(process.handle, &h_mod, sizeof h_mod, &cb_needed))
-            GetModuleBaseName(process.handle, h_mod, process.name,sizeof process.name / sizeof TCHAR);
+            GetModuleBaseName(process.handle, h_mod, process.name, sizeof process.name / sizeof TCHAR);
     }
 
     // Print the process name and ID
@@ -43,6 +99,7 @@ void PrintProcessNameAndID(DWORD processID) {
     if (process.handle)
         CloseHandle(process.handle);
 }
+
 
 void Testbed() {
 
@@ -72,7 +129,29 @@ int main() {
     freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
     freopen_s(reinterpret_cast<FILE**>(stderr), "CONOUT$", "w", stderr);
 
-    Testbed();
+    const std::vector blacklist{
+        std::wstring{ L"cheatengine" },
+        std::wstring{ L"xenos" },
+        std::wstring{ L"extremeinjector" },
+        std::wstring{ L"destroject" }
+    };
 
+    if (const auto processes = find_processes(blacklist)
+        ;  processes) {
+
+        for (const auto& [pid, name] : processes.value()) {
+            std::wcout << pid << name << L'\n';
+            auto h = OpenProcess(PROCESS_ALL_ACCESS, false, 32564);
+            if (not TerminateProcess(h, 0)) {
+                std::wcout << GetLastError() << L'\n';
+            }
+
+
+        }
+    }
+
+    // Testbed();
+
+    Sleep(100'000);
     return 0;
 }

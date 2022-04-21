cc88151325ee18982a88110acc527b9cb6daccbe
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-04-21 05:05:20 +0100

    Enumerate process functionality foundation added to testbed

--- Testbed/main.cpp
+++ Testbed/main.cpp
@@ -4,52 +4,66 @@
 #define NOMINMAX
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <Windows.h>
-#include <WinSock2.h>
-#include <ws2ipdef.h>
-#include <iphlpapi.h>
-#include "sha256.h"
+#include <Psapi.h>
 
 #include <iostream>
 #include <cstdio>
-#include <ranges>
-#include <algorithm>
-#include <string>
 #include <sstream>
-#include <vector>
 
+
+/**
+ * \brief Prints the name of process and it's process ID, or <Untitled> if the process name is unknown
+ *
+ * Adapted from https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocesses
+ * \param processID ID of process to print name and ID of
+ */
+void PrintProcessNameAndID(DWORD processID) {
+
+    struct Process {
+        TCHAR name[MAX_PATH];
+        HANDLE handle;
+    } process {
+        L"<Untitled>",
+        OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,FALSE, processID)
+    };
+
+    // Get the process name
+    if (process.handle != nullptr) {
+        HMODULE h_mod;
+        DWORD cb_needed;
+
+        if (EnumProcessModules(process.handle, &h_mod, sizeof h_mod, &cb_needed))
+            GetModuleBaseName(process.handle, h_mod, process.name,sizeof process.name / sizeof TCHAR);
+    }
+
+    // Print the process name and ID
+    std::wcout << process.name << " PID: " << processID << '\n';
+
+    // Cleanup
+    if (process.handle)
+        CloseHandle(process.handle);
+}
 
 void Testbed() {
-    // Identification
-    // todo: Develop
-    MIB_IPNET_TABLE2* arp;
 
-    const auto result = GetIpNetTable2(AF_UNSPEC, &arp);
-    if (result != NO_ERROR && result != ERROR_NOT_FOUND) return;
-    
-    const std::vector blacklist{ 0x0, 0xff, 0x33, 0x01 };
+    DWORD aProcesses[1024], cbNeeded;
 
-    for (DWORD i = 0; i < arp->NumEntries; ++i) {
-
-        const auto current_bit = static_cast<int>(arp->Table[i].PhysicalAddress[0]);
-        if (std::ranges::find(blacklist, current_bit) != blacklist.end())
-            continue;
-
-        auto row = arp->Table[i];
-
-        SHA256 sha256;
-        std::stringstream ss{};
-
-        for (unsigned long j = 0; j < row.PhysicalAddressLength; j++) {
-            if (j == row.PhysicalAddressLength - 1)
-                ss << std::hex << static_cast<int>(row.PhysicalAddress[j]);
-            else
-                ss << std::hex << static_cast<int>(row.PhysicalAddress[j]) << '-';
-        }
-        std::cout << ss.str() << '\n';
-        std::cout << sha256(ss.str()) << '\n';
+    if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
+        return;
     }
-    
+
+    // Calculate how many process identifiers were returned
+    const DWORD process_count = cbNeeded / sizeof DWORD;
+
+    // Print the name and process identifier for each process
+    for (unsigned i = 0; i < process_count; i++) {
+        if (aProcesses[i] != 0) {
+            PrintProcessNameAndID(aProcesses[i]);
+        }
+    }
+
 }
+
 
 int main() {
 
